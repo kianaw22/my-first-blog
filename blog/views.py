84c3873dc5582afed2098396b5e247f85697c88e
kianaw22 <kiana8089@gmail.com>
2021-12-28 22:00:52 +0330

    phase 5

--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,11 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from blog.models import Post
+from django.shortcuts import render, get_object_or_404
 from django.utils import timezone
 def post_list(request):
     posts = Post.objects.filter().order_by('published_date')
     return render(request, 'blog/post_list.html', {'posts': posts})
+def post_detail(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    return render(request, 'blog/post_detail.html', {'post': post})
