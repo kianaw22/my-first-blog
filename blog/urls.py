d8688cfeffd429b7bb043f53841be5d3076985d0
kianaw22 <kiana8089@gmail.com>
2021-12-29 01:50:03 +0330

    phase 6

--- blog/urls.py
+++ blog/urls.py
@@ -1,9 +1,14 @@
+from django.conf import settings
 from django.urls import path
 from . import views
+from django.conf.urls.static import static # new
 
 urlpatterns = [
     path('', views.post_list, name='post_list'),
     path('post/<int:pk>/', views.post_detail, name='post_detail'),
     path('post/new/', views.post_new, name='post_new'),
     path('post/<int:pk>/edit/', views.post_edit, name='post_edit'),
+
 ]
+if settings.DEBUG: # new
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
