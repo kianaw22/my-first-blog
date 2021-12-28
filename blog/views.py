75e0f1029d1fea5599e521fb9b47aff69194f7cb
kianaw22 <kiana8089@gmail.com>
2021-12-28 14:30:06 +0330

    phase 3

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from blog.models import Post
+from django.utils import timezone
 def post_list(request):
-    return render(request, 'blog/post_list.html', {})
-    #return HttpResponse("Hello")
-# Create your views here.
+    posts = Post.objects.filter().order_by('published_date')
+    return render(request, 'blog/post_list.html', {'posts': posts})
